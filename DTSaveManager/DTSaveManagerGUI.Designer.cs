07e6de809546c9ceefccd2642d9641c8da39283f
nitroBW <nitro@attoch.org>
2021-11-11 02:12:32 +0100

    Fetch Steam data from Registry Keys

---
+++ DTSaveManager/DTSaveManagerGUI.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace DTSaveManager
+{
+    partial class DTSaveManagerGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Size = new System.Drawing.Size(562, 401);
+            this.splitContainer1.SplitterDistance = 174;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(174, 401);
+            this.treeView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 401);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Demon Turf Save Manager [by Nitro]";
+            this.Load += new System.EventHandler(this.Initialization);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ DTSaveManager/DTSaveManagerGUI.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace DTSaveManager
+{
+    public partial class DTSaveManagerGUI : Form
+    {
+        public DTSaveManagerGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Initialization(object sender, EventArgs e)
+        {
+
+            string _saveDirectory = GetPathFromRegistryKeys();
+            Console.WriteLine(_saveDirectory);
+
+            foreach (string file in Directory.EnumerateFiles(_saveDirectory, "*.txt"))
+            {
+                if (File.Exists(file.Replace(".txt", ".json"))) {
+                } else
+                {
+                    //InitializeJson(file);
+                }
+                string contents = File.ReadAllText(file.Replace("txt", "json"));
+                string fileName = Path.GetFileName(file);
+            }
+
+
+        }
+
+        private string GetPathFromRegistryKeys()
+        {
+            string _steamActiveUser = "";
+            string _steamInstallPath = "";
+
+            try
+            {
+                string _steamInstallPathReg = @"SOFTWARE\WOW6432Node\Valve\Steam";
+                RegistryKey _installKey = Registry.LocalMachine.OpenSubKey(_steamInstallPathReg);
+                if (_installKey != null)
+                {
+                    Object o = _installKey.GetValue("InstallPath");
+                    if (o != null)
+                    {
+                        _steamInstallPath = o.ToString();
+                    } else
+                    {
+                        throw new Exception(@"Registry Key 'Computer\HKEY_CURRENT_USER\SOFTWARE\WOW6432Node\Valve\Steam\InstallPath' has no value.");
+                    }
+                }
+                string _steamActiveUserReg = @"SOFTWARE\Valve\Steam\ActiveProcess";
+                RegistryKey _userKey = Registry.CurrentUser.OpenSubKey(_steamActiveUserReg);
+                if (_userKey != null)
+                {
+                    Object o = _userKey.GetValue("ActiveUser");
+                    if (o != null)
+                    {
+                        _steamActiveUser = o.ToString();
+                    } else
+                    {
+                        throw new Exception(@"Registry Key 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam\ActiveProcess\ActiveUser' has no value.");
+                    }
+                }
+
+                return _steamInstallPath + @"\userdata\" + _steamActiveUser + @"\1325900\remote";
+            }
+            catch (Exception ex)
+            {
+                // error, user has to navigate path their own
+                return ex.Message;
+            }
+        }
+    }
+}
