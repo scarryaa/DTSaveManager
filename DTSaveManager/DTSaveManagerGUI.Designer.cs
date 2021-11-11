fcc7ff1b04c98b4c3ecca8edc8a87b0dedd253c9
nitroBW <nitro@attoch.org>
2021-11-11 04:32:55 +0100

    Finish up code

--- DTSaveManager/DTSaveManagerGUI.Designer.cs
+++ DTSaveManager/DTSaveManagerGUI.Designer.cs
@@ -29,10 +29,18 @@ namespace DTSaveManager
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DTSaveManagerGUI));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.saveFileList = new System.Windows.Forms.TreeView();
+            this._duplicate = new System.Windows.Forms.Button();
+            this._name = new System.Windows.Forms.TextBox();
+            this._rename = new System.Windows.Forms.Button();
+            this._applyChanges = new System.Windows.Forms.Button();
+            this._resetChanges = new System.Windows.Forms.Button();
+            this._setActive = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,29 +52,105 @@ namespace DTSaveManager
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
-            this.splitContainer1.Size = new System.Drawing.Size(562, 401);
-            this.splitContainer1.SplitterDistance = 174;
+            this.splitContainer1.Panel1.Controls.Add(this.saveFileList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this._duplicate);
+            this.splitContainer1.Panel2.Controls.Add(this._name);
+            this.splitContainer1.Panel2.Controls.Add(this._rename);
+            this.splitContainer1.Panel2.Controls.Add(this._applyChanges);
+            this.splitContainer1.Panel2.Controls.Add(this._resetChanges);
+            this.splitContainer1.Panel2.Controls.Add(this._setActive);
+            this.splitContainer1.Size = new System.Drawing.Size(622, 291);
+            this.splitContainer1.SplitterDistance = 401;
             this.splitContainer1.TabIndex = 0;
             // 
-            // treeView1
+            // saveFileList
             // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(174, 401);
-            this.treeView1.TabIndex = 0;
+            this.saveFileList.CheckBoxes = true;
+            this.saveFileList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.saveFileList.Location = new System.Drawing.Point(0, 0);
+            this.saveFileList.Name = "saveFileList";
+            this.saveFileList.Size = new System.Drawing.Size(401, 291);
+            this.saveFileList.TabIndex = 0;
+            this.saveFileList.BeforeCheck += new System.Windows.Forms.TreeViewCancelEventHandler(this.CheckDisabler);
+            this.saveFileList.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.SaveListSelectionChanged);
             // 
-            // Form1
+            // _duplicate
+            // 
+            this._duplicate.Enabled = false;
+            this._duplicate.Location = new System.Drawing.Point(3, 85);
+            this._duplicate.Name = "_duplicate";
+            this._duplicate.Size = new System.Drawing.Size(211, 23);
+            this._duplicate.TabIndex = 5;
+            this._duplicate.Text = "Duplicate";
+            this._duplicate.UseVisualStyleBackColor = true;
+            this._duplicate.Click += new System.EventHandler(this.DuplicateSelection);
+            // 
+            // _name
+            // 
+            this._name.Enabled = false;
+            this._name.Location = new System.Drawing.Point(4, 59);
+            this._name.Name = "_name";
+            this._name.Size = new System.Drawing.Size(210, 20);
+            this._name.TabIndex = 4;
+            // 
+            // _rename
+            // 
+            this._rename.Enabled = false;
+            this._rename.Location = new System.Drawing.Point(3, 34);
+            this._rename.Name = "_rename";
+            this._rename.Size = new System.Drawing.Size(211, 23);
+            this._rename.TabIndex = 3;
+            this._rename.Text = "Rename";
+            this._rename.UseVisualStyleBackColor = true;
+            this._rename.Click += new System.EventHandler(this.RenameSelection);
+            // 
+            // _applyChanges
+            // 
+            this._applyChanges.Location = new System.Drawing.Point(3, 236);
+            this._applyChanges.Name = "_applyChanges";
+            this._applyChanges.Size = new System.Drawing.Size(211, 23);
+            this._applyChanges.TabIndex = 2;
+            this._applyChanges.Text = "Apply Changes";
+            this._applyChanges.UseVisualStyleBackColor = true;
+            this._applyChanges.Click += new System.EventHandler(this.WriteChanges);
+            // 
+            // _resetChanges
+            // 
+            this._resetChanges.Location = new System.Drawing.Point(3, 265);
+            this._resetChanges.Name = "_resetChanges";
+            this._resetChanges.Size = new System.Drawing.Size(211, 23);
+            this._resetChanges.TabIndex = 1;
+            this._resetChanges.Text = "Reset Changes";
+            this._resetChanges.UseVisualStyleBackColor = true;
+            this._resetChanges.Click += new System.EventHandler(this.ResetChanges);
+            // 
+            // _setActive
+            // 
+            this._setActive.Enabled = false;
+            this._setActive.Location = new System.Drawing.Point(3, 5);
+            this._setActive.Name = "_setActive";
+            this._setActive.Size = new System.Drawing.Size(211, 23);
+            this._setActive.TabIndex = 0;
+            this._setActive.Text = "Set Active";
+            this._setActive.UseVisualStyleBackColor = true;
+            this._setActive.Click += new System.EventHandler(this.SetActive);
+            // 
+            // DTSaveManagerGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 401);
+            this.ClientSize = new System.Drawing.Size(622, 291);
             this.Controls.Add(this.splitContainer1);
-            this.Name = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DTSaveManagerGUI";
             this.Text = "Demon Turf Save Manager [by Nitro]";
             this.Load += new System.EventHandler(this.Initialization);
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -76,7 +160,13 @@ namespace DTSaveManager
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView saveFileList;
+        private System.Windows.Forms.Button _setActive;
+        private System.Windows.Forms.Button _duplicate;
+        private System.Windows.Forms.TextBox _name;
+        private System.Windows.Forms.Button _rename;
+        private System.Windows.Forms.Button _applyChanges;
+        private System.Windows.Forms.Button _resetChanges;
     }
 }
 
--- DTSaveManager/DTSaveManagerGUI.cs
+++ DTSaveManager/DTSaveManagerGUI.cs
@@ -17,6 +17,8 @@ namespace DTSaveManager
     public partial class DTSaveManagerGUI : Form
     {
         private List<SaveMetadata> _saveMetadata;
+        private bool _initialized = false;
+        private string _saveDirectory;
         public DTSaveManagerGUI()
         {
             InitializeComponent();
@@ -24,35 +26,32 @@ namespace DTSaveManager
 
         private void Initialization(object sender, EventArgs e)
         {
-            _saveMetadata = new List<SaveMetadata>();
-            string _saveDirectory = GetPathFromRegistryKeys();
-            InitializeFiles(_saveDirectory);
+            _initialized = false;
+            Initializer();
         }
 
-        private void InitializeFiles(string _saveDirectory)
+        private void Initializer()
         {
-            foreach (string file in Directory.EnumerateFiles(_saveDirectory, "*.txt"))
-            {
-                string fileName = Path.GetFileName(file);
-                string jsonPath = file.Replace(".txt", ".dtsm");
-                SaveMetadata data;
+            _initialized = false;
 
-                if (File.Exists(jsonPath))
-                {
-                    data = JsonConvert.DeserializeObject<SaveMetadata>(File.ReadAllText(jsonPath));
-                }
-                else
-                {
-                    data = new SaveMetadata()
-                    {
-                        fileName = fileName,
-                        nickName = "",
-                        path = file,
-                        active = false
-                    };
-                    File.WriteAllText(jsonPath, JsonConvert.SerializeObject(data));
-                }
-                _saveMetadata.Add(data);
+            _saveMetadata = new List<SaveMetadata>();
+            _saveDirectory = GetPathFromRegistryKeys();
+            InitializeFiles(_saveDirectory);
+
+            InitializeTreeView();
+
+            _setActive.Enabled = false;
+            _rename.Enabled = false;
+            _name.Enabled = false;
+            _name.Text = "";
+            _duplicate.Enabled = false;
+
+            saveFileList.SelectedNode = null;
+            _initialized = true;
+
+            if (_saveMetadata.FindAll(m => m.active == true).Count != 1)
+            {
+                _saveMetadata.Find(m => m.fileName == "DTSaveData.txt").active = true;
             }
         }
 
@@ -71,7 +70,8 @@ namespace DTSaveManager
                     if (o != null)
                     {
                         _steamInstallPath = o.ToString();
-                    } else
+                    }
+                    else
                     {
                         throw new Exception(@"Registry Key 'Computer\HKEY_CURRENT_USER\SOFTWARE\WOW6432Node\Valve\Steam\InstallPath' has no value.");
                     }
@@ -84,7 +84,8 @@ namespace DTSaveManager
                     if (o != null)
                     {
                         _steamActiveUser = o.ToString();
-                    } else
+                    }
+                    else
                     {
                         throw new Exception(@"Registry Key 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam\ActiveProcess\ActiveUser' has no value.");
                     }
@@ -97,6 +98,173 @@ namespace DTSaveManager
                 // error, user has to navigate path their own
                 return ex.Message;
             }
+        }
+
+        private void InitializeFiles(string _saveDirectory)
+        {
+            // move files to _dtsm folder
+            foreach (string file in Directory.EnumerateFiles(_saveDirectory, "*.txt"))
+            {
+                string fileName = Path.GetFileName(file);
+                Directory.CreateDirectory(_saveDirectory + @"\_dtsm");
+
+                if (file.Contains(@"\DTSaveData.txt"))
+                {
+                    if (!File.Exists(_saveDirectory + @"\_dtsm\" + fileName))
+                    {
+                        File.Copy(file, _saveDirectory + @"\_dtsm\" + fileName);
+                    }
+                } else
+                {
+                    File.Move(file, _saveDirectory + @"\_dtsm\" + fileName);
+                }
+            }
+            // scan _dtsm folder
+            foreach (string file in Directory.EnumerateFiles(_saveDirectory + "\\_dtsm", "*.txt"))
+            {
+                string fileName = Path.GetFileName(file);
+
+                // if inactive DTSaveData.txt exists, ignore it
+                string jsonPath = file.Replace(".txt", ".dtsm");
+                SaveMetadata data;
+
+                if (File.Exists(jsonPath))
+                {
+                    data = JsonConvert.DeserializeObject<SaveMetadata>(File.ReadAllText(jsonPath));
+                }
+                else
+                {
+                    data = new SaveMetadata()
+                    {
+                        fileName = fileName,
+                        nickName = "",
+                        path = _saveDirectory + @"\_dtsm\" + fileName,
+                        active = fileName == "DTSaveData.txt"
+                    };
+                    File.WriteAllText(jsonPath, JsonConvert.SerializeObject(data));
+                }
+                _saveMetadata.Add(data);
+            }
+
+            if (_saveMetadata.FindAll(m => m.active == true).Count != 1)
+            {
+                _saveMetadata.Find(m => m.fileName == "DTSaveData.txt").active = true;
+            }
+        }
+
+        private void InitializeTreeView()
+        {
+            saveFileList.Nodes.Clear();
+            _saveMetadata.ForEach(data => {
+
+                TreeNode node = new TreeNode()
+                {
+                    Name = data.fileName,
+                    Tag = (object)data,
+                    Text = data.nickName.Length > 0 && data.nickName != data.fileName ? "\"" + data.nickName + "\" (" + data.fileName + ")" : data.fileName,
+                    Checked = data.active,
+                };
+                Console.WriteLine(data.active);
+
+                saveFileList.Nodes.Add(node);
+
+            });
+        }
+
+        private void ResetChanges(object sender, EventArgs e)
+        {
+            Initializer();
+        }
+
+        private void SaveListSelectionChanged(object sender, TreeViewEventArgs e)
+        {
+            if (!_initialized) return;
+
+            SaveMetadata data = (SaveMetadata)e.Node.Tag;
+
+            _setActive.Enabled = !data.active;
+            _rename.Enabled = true;
+            _name.Enabled = true;
+            _name.Text = data.nickName.Length > 0 ? data.nickName : data.fileName;
+            _duplicate.Enabled = true;
+        }
+
+        private void RenameSelection(object sender, EventArgs e)
+        {
+            SaveMetadata data = (SaveMetadata)saveFileList.SelectedNode.Tag;
+            data.nickName = _name.Text;
+            saveFileList.SelectedNode.Text = data.nickName.Length > 0 && data.nickName != data.fileName ? "\"" + data.nickName + "\" (" + data.fileName + ")" : data.fileName;
+        }
+
+        private void SetActive(object sender, EventArgs e)
+        {
+            _initialized = false;
+            foreach(TreeNode node in saveFileList.Nodes)
+            {
+                // Disable all (other) nodes
+                node.Checked = false;
+                SaveMetadata _data = (SaveMetadata)node.Tag;
+                _data.active = false;
+            }
+
+            SaveMetadata data = (SaveMetadata)saveFileList.SelectedNode.Tag;
+            data.active = true;
+            saveFileList.SelectedNode.Checked = true;
+            _setActive.Enabled = false;
+            _initialized = true;
+
+            InitializeTreeView();
+        }
+
+        private void DuplicateSelection(object sender, EventArgs e)
+        {
+            SaveMetadata data = (SaveMetadata)saveFileList.SelectedNode.Tag;
+            string oldPath = data.path;
+
+            SaveMetadata newData = new SaveMetadata();
+
+            newData.fileName = data.fileName.Replace(".txt", "_duplicate.txt");
+            newData.path = data.path.Replace(".txt", "_duplicate.txt");
+            newData.active = false;
+            newData.nickName = data.nickName.Length > 0 ? data.nickName + "_duplicate" : "";
+
+            if (!File.Exists(newData.path))
+            {
+                File.Copy(oldPath, newData.path, true);
+                File.Copy(oldPath.Replace(".txt", ".dtsm"), newData.path.Replace(".txt", ".dtsm"), true);
+
+                _saveMetadata.Add(newData);
+                InitializeTreeView();
+            } else
+            {
+                // throw error
+                MessageBox.Show("File already exists!");
+            }
+        }
+
+        private void WriteChanges(object sender, EventArgs e)
+        {
+            _saveMetadata.ForEach(data => {
+                File.WriteAllText(data.path.Replace(".txt", ".dtsm"), JsonConvert.SerializeObject(data));
+            });
+
+            foreach (TreeNode node in saveFileList.Nodes)
+            {
+                SaveMetadata _data = (SaveMetadata)node.Tag;
+                if (_data.active && node.Checked)
+                {
+                    if (File.Exists(_saveDirectory + @"\DTSaveData.txt"))
+                    {
+                        File.Copy(_saveDirectory + @"\_dtsm\" + _data.fileName, _saveDirectory + @"\DTSaveData.txt", true);
+                    }
+                }
+            }
+        }
+
+        private void CheckDisabler(object sender, TreeViewCancelEventArgs e)
+        {
+            if (!_initialized) return;
+            e.Cancel = true;
         }
     }
 }
